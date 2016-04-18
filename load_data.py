e050fb4c469bde61daaab5efd2cfdf7ec5eda322
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2016-04-18 18:15:09 +0200

    started on sliding window

--- load_data.py
+++ load_data.py
@@ -7,7 +7,7 @@ from PIL import Image
 from decimal import Decimal
 
 # 'svc' or 'dt'
-classifier_type = 'svc'
+classifier_type = 'dt'
 
 def load_data():
     rootdir = os.getcwd()+'/chars74k-lite'
@@ -62,11 +62,13 @@ preprocessing_type = 'scale'
 test = preprocess_data(test, preprocessing_type)
 training = preprocess_data(training, preprocessing_type)
 
+# Lists for labels and data
 training_target = []
 x_training = []
 test_target = []
 x_test = []
 
+# Insert data into the lists
 for label in range(len(training)):
     for image in training[label]:
         training_target.append(label)
@@ -78,12 +80,39 @@ for label in range(len(test)):
         x_test.append(image)
 
 
+# Choose classifier type
 if classifier_type=='svc':
     classifier = svm.SVC(gamma=0.005) #gamma = 0.004
 elif classifier_type=='dt':
     classifier = tree.DecisionTreeClassifier(max_depth=20)
 
+# Train and test
 classifier.fit(x_training, training_target)
 predicted = classifier.predict(x_test)
 
+# Print result
 print("Classification report for classifier %s:\n%s\n" % (classifier, metrics.classification_report(test_target, predicted)))
+
+
+# Window slider
+path = os.getcwd() + "/slider_test-detecting.jpg"
+image = Image.open(path)
+image = image.convert('L')
+print image
+image = list(image.getdata()) 
+image = [float(x) for x in image]
+image = np.array(image)
+image = ndimage.median_filter(image, 3)
+image = preprocessing.scale(image)
+width = 159
+height = 66
+
+for x in range(width-20):
+    for y in range(height-20):
+        cropped_image = []
+        for dy in range(20):
+            start = x + (y+dy)*width
+            cropped_image.append(image[start:start+20])
+        predicted = classifier.predict(cropped_image)
+        print predicted
+
