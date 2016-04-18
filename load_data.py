eb69cbd4f71ccd911c9bf07456cd983ab3b82cb0
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2016-04-18 17:29:46 +0200

    training and testing classifier done

--- load_data.py
+++ load_data.py
@@ -1,7 +1,13 @@
 import sklearn
+from sklearn import datasets, svm, tree, metrics, preprocessing
+from scipy import ndimage
+import numpy as np
 import os
 from PIL import Image
 from decimal import Decimal
+
+# 'svc' or 'dt'
+classifier_type = 'svc'
 
 def load_data():
     rootdir = os.getcwd()+'/chars74k-lite'
@@ -26,18 +32,58 @@ def load_data():
             i += 1
     return test_images, training_images
 
-def preprocess_data(image_list):
-    for label in image_list:
-        for image in label:
-            for p in range(len(image)):
-                image[p] = image[p]/255.0
+def preprocess_data(data, prep_type='noise'):
+    output = []
+    if prep_type=='noise':
+        for labels in data:
+            tmp = []
+            for image in labels:
+                tmp.append(ndimage.median_filter(image, 3))
+            output.append(tmp)
+    elif prep_type=='scale':
+        for labels in data:
+            tmp = []
+            for image in labels:
+                tmp.append(preprocessing.scale(image))
+            output.append(tmp)
+    return output
 
+# Load data
 test, training =load_data()
-preprocess_data(test)
-preprocess_data(training)
+# Convert to numpy arrays
+test = np.array(test)
+training = np.array(training)
+
+# Preprocess data
+preprocessing_type = 'noise'
+test = preprocess_data(test, preprocessing_type)
+training = preprocess_data(training, preprocessing_type)
+preprocessing_type = 'scale'
+test = preprocess_data(test, preprocessing_type)
+training = preprocess_data(training, preprocessing_type)
+
+training_target = []
+x_training = []
+test_target = []
+x_test = []
+
+for label in range(len(training)):
+    for image in training[label]:
+        training_target.append(label)
+        x_training.append(image)
+
+for label in range(len(test)):
+    for image in test[label]:
+        test_target.append(label)
+        x_test.append(image)
 
 
-print test[0][0][0]
-print test[1][0][0]
+if classifier_type=='svc':
+    classifier = svm.SVC(gamma=0.005) #gamma = 0.004
+elif classifier_type=='dt':
+    classifier = tree.DecisionTreeClassifier(max_depth=20)
 
+classifier.fit(x_training, training_target)
+predicted = classifier.predict(x_test)
 
+print("Classification report for classifier %s:\n%s\n" % (classifier, metrics.classification_report(test_target, predicted)))
