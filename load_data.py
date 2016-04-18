dc88b43d663f218fe6b3a1b5791510830d04ec6c
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2016-04-18 15:34:32 +0200

    loading data now works

--- load_data.py
+++ load_data.py
@@ -5,8 +5,12 @@ from decimal import Decimal
 
 def load_data():
     rootdir = os.getcwd()+'/chars74k-lite'
-    test_images = [[]]*26
-    training_images = [[]]*26
+    test_images = []
+    training_images = []
+
+    for i in range(26):
+        test_images.append([])
+        training_images.append([])
 
     i = 0
     for subdir, dirs, files in os.walk(rootdir):
@@ -16,22 +20,24 @@ def load_data():
             im = Image.open(os.path.join(subdir, file))
             data = list(im.getdata())
             if i%2:
-                test_images[label].append(data)
+                test_images[label].append([float(x) for x in data])
             else:
-                training_images[label].append(data)
+                training_images[label].append([float(x) for x in data])
             i += 1
     return test_images, training_images
 
-def process_data(image_list):
-    for x in range(len(image_list)):
-        for y in range(len(image_list[x])):
-            for i in range(len(image_list[x][y])):
-                image_list[x][y][i] = str(float(image_list[x][y][i])/255)
-                
+def preprocess_data(image_list):
+    for label in image_list:
+        for image in label:
+            for p in range(len(image)):
+                image[p] = image[p]/255.0
+
 test, training =load_data()
-process_data(test)
+preprocess_data(test)
+preprocess_data(training)
 
-print test[0][0]
 
+print test[0][0][0]
+print test[1][0][0]
 
 
