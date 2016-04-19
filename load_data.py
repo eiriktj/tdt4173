84182e7a48a84664e04513fe8dc13c839d35d993
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2016-04-19 11:51:01 +0200

    testing treshold

--- load_data.py
+++ load_data.py
@@ -1,13 +1,14 @@
 import sklearn
 from sklearn import datasets, svm, tree, metrics, preprocessing
+from sklearn.naive_bayes import GaussianNB
 from scipy import ndimage
 import numpy as np
 import os
 from PIL import Image
 from decimal import Decimal
 
-# 'svc' or 'dt'
-classifier_type = 'dt'
+# 'svc' | 'dt' | 'gnb'
+classifier_type = 'svc'
 
 def load_data():
     rootdir = os.getcwd()+'/chars74k-lite'
@@ -82,9 +83,11 @@ for label in range(len(test)):
 
 # Choose classifier type
 if classifier_type=='svc':
-    classifier = svm.SVC(gamma=0.005) #gamma = 0.004
+    classifier = svm.SVC(gamma=0.005, probability=True) #gamma = 0.004
 elif classifier_type=='dt':
     classifier = tree.DecisionTreeClassifier(max_depth=20)
+elif classifier_type=='gnb':
+    classifier = GaussianNB()
 
 # Train and test
 classifier.fit(x_training, training_target)
@@ -95,7 +98,7 @@ print("Classification report for classifier %s:\n%s\n" % (classifier, metrics.cl
 
 
 # Window slider
-path = os.getcwd() + "/slider_test-detecting.jpg"
+path = os.getcwd() + "/abc.jpg"
 image = Image.open(path)
 image = image.convert('L')
 print image
@@ -104,15 +107,22 @@ image = [float(x) for x in image]
 image = np.array(image)
 image = ndimage.median_filter(image, 3)
 image = preprocessing.scale(image)
-width = 159
-height = 66
+width = 100
+height = 20
 
-for x in range(width-20):
-    for y in range(height-20):
+for y in range(height-19):
+    for x in range(0, width-19, 2):
         cropped_image = []
         for dy in range(20):
             start = x + (y+dy)*width
-            cropped_image.append(image[start:start+20])
+            for dx in range(20):
+                cropped_image.append(image[start+dx])
         predicted = classifier.predict(cropped_image)
-        print predicted
+        prob = classifier.predict_proba(cropped_image)
+        print prob
+        if prob[0][predicted]>0.50:
+            print chr(predicted+97)
+        #if classifier_type=='gnb':
+        #    print classifier.predict_proba(cropped_image)
+
 
